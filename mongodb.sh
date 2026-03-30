2b1c80127902567453e944aba10e8f2769d98cff
Sumakarkatla89 <sumakarkatla89.com>
2026-03-30 22:25:58 +0530

    iam pushing my code

--- mongodb.sh
+++ mongodb.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/script.log"
+
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 ... ${R}FAILED${N}"
+    exit 1
+  else
+    echo -e "$2 ... ${G}SUCCESS${N}"
+  fi
+}
+
+if [ $ID -ne 0 ]
+then 
+  echo -e "${R}ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER${N}"
+  exit 1
+else
+  echo -e "${G}you are root user${N}"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copied mongodb repo"
