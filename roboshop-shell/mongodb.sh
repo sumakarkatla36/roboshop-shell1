361817412a3f9b71d3506e5bb21de0843d0d4a34
Sumakarkatla89 <sumakarkatla89.com>
2026-04-22 15:01:27 +0530

    iam pushing my code

---
+++ roboshop-shell/mongodb.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/script.log"
+
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 ... ${R}FAILED${N}"
+    exit 1
+  else
+    echo -e "$2 ... ${G}SUCCESS${N}"
+  fi
+}
+
+if [ $ID -ne 0 ]
+then 
+  echo -e "${R}ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER${N}"
+  exit 1
+else
+  echo -e "${G}you are root user${N}"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copied Mongodb repo"
+
+dnf install mongodb-org -y
+
+VALIDATE $? "Installing MOngodb"  &>> $LOGFILE
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling Mongodb"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "starting mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Remote access to Mongodb"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting Mongodb"
+
+
+
+
+
